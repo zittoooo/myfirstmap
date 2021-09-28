10e75fea8c249aca5375d493c9a0b96ec3b8202d
jiholee <stellar7484@naver.com>
2021-09-28 16:39:40 +0900

    placesList

--- myfirstmap/public/data/data.js
+++ myfirstmap/public/data/data.js
@@ -1,16 +1,16 @@
 var data = [
 	{
-	  title : "서울", 
-	  content: "서울 남산", 
+	  company : "남산", 
+	  address: "서울 남산", 
 	  date : "2020-07-10", 
 	  lat : 37.55504960115933,
 	  lng: 126.98114112443173
 	},  
 	{
-	  title : "서울", 
-	  content: "서울 용산", 
+	  company : "서울", 
+	  address: "서울 용산", 
 	  date : "2020-07-11", 
 	  lat : 37.5254148, 
 	  lng: 126.9798002
-	}
+	},
 ]
